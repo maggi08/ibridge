b362a9991bea807d94bba5f9a8181e3c37c89fe8
Magzhan Mussaiyn <maggi08mus@gmail.com>
2021-10-31 16:26:43 +0600

    partners

--- nuxt.config.js
+++ nuxt.config.js
@@ -102,7 +102,7 @@ export default {
   css: ['~/assets/main.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['~/plugins/swiper'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,7 @@
     <Services />
     <Achivements />
     <Study />
+    <Partners />
   </div>
 </template>
 
