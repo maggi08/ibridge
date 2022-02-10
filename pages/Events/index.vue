ed5e517f83b68d39453118595245c3ba6486ce43
Magzhan Mussaiyn <maggi08mus@gmail.com>
2022-02-11 01:27:39 +0600

    events page and request modal component

--- pages/Events/index.vue
+++ pages/Events/index.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class=""></div>
+  <div class="h-100 w-100 d-flex align-center justify-center">
+    <RequestModal events />
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'empty',
+  layout: 'events',
+  created() {},
 }
 </script>
 
