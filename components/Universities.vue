b3da9ea2703fc9d6b360258fdf66340456e616d5
Magzhan Mussaiyn <maggi08mus@gmail.com>
2022-02-15 12:03:45 +0600

    filter

--- components/Universities.vue
+++ components/Universities.vue
@@ -231,7 +231,8 @@ export default {
     },
   },
   created() {
-    this.partneers = this.partners
+    // this.partneers = this.partners
+    this.toggleTab(1)
     this.test = [...this.partners].splice(0, this.pageSize)
   },
   methods: {
